d487c39daf70a1b663d1dfe997bdc90a26962426
DulanMahesh <dulansurawera@gmail.com>
2024-08-02 11:09:26 +0200

    Convolution

---
+++ convolution_in_openCV.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the kernel
+kernel_size = 5
+# Create a 5x5 kernel with all elements equal to 1.
+kernel = np.ones((kernel_size, kernel_size), dtype=np.float32) / kernel_size ** 2
+
+# Perform convolution
+filename = 'kitten.jpg'  # Ensure this file is in your project directory or provide the correct path
+image = cv2.imread(filename)
+
+# Check if the image was loaded properly
+if image is None:
+    print(f"Error: Unable to load image at {filename}")
+else:
+    dst = cv2.filter2D(image, ddepth=-1, kernel=kernel)
+
+    plt.figure(figsize=[20, 10])
+    plt.subplot(121),plt.axis('off'),plt.imshow(image[:, :, ::-1]),plt.title("Original Image")
+
+    plt.subplot(122),plt.axis('off'),plt.imshow(dst[:, :, ::-1]),plt.title("Convolution Result")
+
+    plt.show()
